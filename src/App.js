1c2c4dc918d6b0581aa6ee4c36878bcaabe869fe
Joseph Jayeoba <josephbusayojayeoba@gmail.com>
2022-12-24 20:55:44 +0100

    Added test for button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [btnColor, setBtnColor] = useState('green')
+  const newBtnColor = btnColor === 'green' ? 'blue' : 'green'
+
   return (
     <div className="App">
       <h1>Test App</h1>
+
+      <button
+        style={{ backgroundCOlor: btnColor}}
+        onClick={() => setBtnColor(newBtnColor)}
+      >
+        Change button color to {newBtnColor}
+      </button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,22 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/react/user-event';
 import App from './App';
 
 test('The header renders with the correct text', () => {
   render(<App />);
   const headerEl = screen.getByRole('heading')
   expect(headerEl.textContent).toBe('Test App')
+
  /*  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument(); */
 });
+
+text("button changes color when clicked", () => {
+  render(<App />)
+  const colorBtn = screen.getByRole('button')
+
+  userEvent.click(colorBtn)
+
+  expect(colorBtn).toHaveStyle({backgroundColor: 'blue'})
+  expect(colorBtn.textContent).toBe("Change button color to green")
+})
